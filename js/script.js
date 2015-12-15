ac89fc2e8713689a08c97fc4f6da57470218a2f2
David Lorente <david.l.castilla@gmail.com>
2015-12-14 23:04:34 -0500

    add some style and refactor js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ var itunesUrl = 'https://itunes.apple.com/search';
 var inputSearch = document.getElementById('searchValue');
 var contentDiv = document.getElementById('content');
 
+$('#searchValue').on('keyup', function(e) {
+    if (e.keyCode === 13) {
+        searchPodcast();
+    }
+});
 
 Storage.prototype.setObj = function(key, obj) {
     return this.setItem(key, JSON.stringify(obj))
@@ -21,12 +26,15 @@ if (!localStorage.favorites){
   localStorage.setObj("favorites", [])
 }
 
-if(localStorage.favorites){
-  var favorites = localStorage.getObj("favorites");
-  _.forEach(favorites, function(podcast){
-    makePodcastPreview(podcast);
-  })
+function showFavorites(){
+  if(localStorage.favorites){
+    var favorites = localStorage.getObj("favorites");
+    _.forEach(favorites, function(podcast){
+      makePodcastPreview(podcast);
+    })
+  }
 }
+showFavorites()
 
 function containsObject(obj, list) {
   var i;
@@ -69,32 +77,8 @@ function getXML(feedUrl, callback){
 var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + feedUrl + '"') + '&format=xml&callback=?';
 var urltest = 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?';
   $.getJSON(yql).done(function (data) {
-    // console.log(data.results[0])
     xmlDoc = $.parseXML(data.results[0])
-    // console.log(xmlDoc)
-    var items=$('channel > item',data.results[0]);
-    // console.log(items)
-
-    _.forEach(items, function(item) {
-      // console.log(item)
-        var date = document.createElement("h1");
-            date.innerHTML = $('title',item).text();
-
-        var audio = document.createElement("audio");
-            audio.setAttribute("controls", "controls")
-        var source = document.createElement("source");
-        source.src = $('enclosure',item).attr('url');
-        audio.appendChild(source)
-
-
-        var link=$('<a/>').attr('href',$('enclosure',item).attr('url'))
-              .text($('title',item).text());
-
-        contentDiv.appendChild(date);
-        contentDiv.appendChild(audio);
-      // }
-    });
-    // callback(data)
+    callback(xmlDoc)
   });
 }
 
@@ -145,28 +129,30 @@ function makePodcastPreview(obj){
   return obj
 }
 
-function appendEpisodes(episode){
-  // console.log(episode)
-  // var div = document.createElement("div");
-  // var img = document.createElement("img");
-  // var title = document.createElement("h2");
-  // var autor = document.createElement("h3");
-  //   img.src = obj.artworkUrl100;
-  //   title.innerHTML = obj.collectionName
-  //   autor.innerHTML = obj.artistName
-  // div.className = "pod-preview"
-  // div.setAttribute("onclick", "getEpisodes("+"'"+obj.feedUrl+"'"+")")
-  // div.appendChild(img)
-  // div.appendChild(title)
-  // div.appendChild(autor)
-  // contentDiv.appendChild(div);
-  // return obj
+function refresh(){
+  clearE(contentDiv)
+  showFavorites();
+}
+
+function appendEpisodes(episodes){
+  _.forEach(episodes, function(episode) {
+    var date = document.createElement("h1");
+      date.innerHTML = $('title',episode).text();
+    var audio = document.createElement("audio");
+      audio.setAttribute("controls", "controls")
+    var source = document.createElement("source");
+      source.src = $('enclosure',episode).attr('url');
+      audio.appendChild(source)
+    var link=$('<a/>').attr('href',$('enclosure',episode).attr('url'))
+          .text($('title',episode).text());
+    contentDiv.appendChild(date);
+    contentDiv.appendChild(audio);
+  })
 }
 
 function searchPodcast(){
   var url = itunesUrl + '?media=podcast&term=' + inputSearch.value;
   jsonp(url, function(res){
-    // console.log(res.results)
     clearE(contentDiv)
     _.forEach(res.results, function(obj) {
       makePodcastPreview(obj)
@@ -176,10 +162,8 @@ function searchPodcast(){
 
 function getEpisodes(feedUrl){
   getXML(feedUrl, function(res){
-    // console.log(res.responseData.feed.entries)
     clearE(contentDiv)
-    _.forEach(res.responseData.feed.entries, function(episode) {
-      appendEpisodes(episode)
-    });
+    var items=$('channel > item',res);
+    appendEpisodes(items)
   })
 }
